18d542e305ce933d2a587170879d48f953fc2854
zax80 <ispahiyski@gmail.com>
2020-08-06 14:03:49 +0300

    Line 17 '<=" to "<"

--- AssignmentTeamLongestPeriod/OverlapCase.cs
+++ AssignmentTeamLongestPeriod/OverlapCase.cs
@@ -14,7 +14,7 @@ namespace AssignmentTeamLongestPeriod
             this.Employee1ID = e1.EmpID;
             this.Employee2ID = e2.EmpID;
             this.ProjectID = e1.ProjectID;
-            this.DaysOn = (e1.DateFrom <= e2.DateTo) ? Convert.ToInt32((e2.DateTo - e1.DateFrom).TotalDays) : Convert.ToInt32((e1.DateTo - e2.DateFrom).TotalDays);
+            this.DaysOn = (e1.DateFrom < e2.DateTo) ? Convert.ToInt32((e2.DateTo - e1.DateFrom).TotalDays) : Convert.ToInt32((e1.DateTo - e2.DateFrom).TotalDays);
         }
     }
 }
